172ad57b45294403c802d86a78b2feab408d1c03
Nuthan B <NuthanB@protonmail.com>
2024-01-14 14:30:27 +0530

    Added the search option

--- app/src/Pages/PutAttendance/PutAttendance.js
+++ app/src/Pages/PutAttendance/PutAttendance.js
@@ -2,13 +2,16 @@ import React from "react";
 import DatePicker from "../../Components/DatePicker";
 import "./PutAttendancs.css";
 
+
 const PutAttendance = () => {
   return (
+    <div>
     <div className="staff-attendance-wrapper">
       <DatePicker
         minDate={new Date(2022, 4, 22)}
         maxDate={new Date(2024, 10, 22)}
       />
+    </div>
     </div>
   );
 };
